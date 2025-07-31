c6f046c7a0d2f713ff5693937da96114598986c6
Muzammil Hussain <smhz101@users.noreply.github.com>
2025-07-31 18:37:26 +0500

    Introduce event bus for bid events

--- includes/class-wpam-loader.php
+++ includes/class-wpam-loader.php
@@ -9,6 +9,13 @@ class WPAM_Loader {
         }
 
         // Init classes
+        // Register event listeners
+        WPAM_Event_Bus::register( new WPAM_Notifications() );
+        $pusher_provider = new WPAM_Pusher_Provider();
+        if ( $pusher_provider->is_active() ) {
+            WPAM_Event_Bus::register( $pusher_provider );
+        }
+
         new WPAM_Auction();
         new WPAM_Bid();
         new WPAM_Watchlist();
